e8bdc710f032e1a64bec74706a6cd8dfc71675db
Chris McDonald <McDonaldChristopherC@gmail.com>
2026-03-07 20:13:37 -0500

    Hopper control (#9)
    * adding buttons
    
    * add hopper trigger tied to intake and indexer
    
    * edited indexer sim
    
    * changed to motion magic
    
    * updated indexer commands
    
    ---------
    
    Co-authored-by: Finney Student <finneystudent@gmail.com>

--- src/main/java/frc/robot/subsystems/Indexer.java
+++ src/main/java/frc/robot/subsystems/Indexer.java
@@ -57,11 +57,11 @@ public class Indexer extends SubsystemBase {
     }
 
     public Command runIndexer(Supplier<AngularVelocity> speed) {
-        return startEnd(() -> setIndexerSpeed(speed), () -> indexerStop());
+        return runOnce(() -> setIndexerSpeed(speed));
     }
 
     public Command runIndexer() {
-        return startEnd(() -> setIndexerSpeed(), () -> indexerStop());
+        return runOnce(() -> setIndexerSpeed());
     }
 
     public Command runStopIndexer() {
