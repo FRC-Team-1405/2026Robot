ce02726543357ab39d382b864327b077fa021b31
Dalton <daltonjkennedy@gmail.com>
2026-03-21 16:17:07 -0400

    adding auto brake FS

--- src/main/java/frc/robot/constants/FeatureSwitches.java
+++ src/main/java/frc/robot/constants/FeatureSwitches.java
@@ -7,4 +7,5 @@ package frc.robot.constants;
 public class FeatureSwitches {
 
     public static final boolean ENABLE_SUBSYSTEM_LOGGING = true;
+    public static final boolean BRAKE_WHILE_SHOOTING = false;
 }
