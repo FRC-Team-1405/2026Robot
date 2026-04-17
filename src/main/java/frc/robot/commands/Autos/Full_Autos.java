971d180262a09662e898ba1304dae741ec84edff
Stephen Cerbone <importsjc@gmail.com>
2026-04-16 20:48:14 -0400

    adding back import i removed

--- src/main/java/frc/robot/commands/Autos/Full_Autos.java
+++ src/main/java/frc/robot/commands/Autos/Full_Autos.java
@@ -5,6 +5,8 @@
 package frc.robot.commands.Autos;
 
 import com.pathplanner.lib.auto.NamedCommands;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
